28d0048fc73550922391ffcb735621b62f4af143
Drakenmike <163910533+Drakenmike@users.noreply.github.com>
2025-05-15 15:56:16 -0400

    Update sticker-maker.js

--- plugins/sticker-maker.js
+++ plugins/sticker-maker.js
@@ -57,7 +57,7 @@ cmd(
     async (conn, mek, m, { quoted, args, q, reply, from }) => {
         if (!mek.quoted) return reply(`*Reply to any Image or Video, Sir.*`);
         let mime = mek.quoted.mtype;
-        let pack = Config.STICKER_NAME || "ᴍᴇɢᴀʟᴏᴅᴏɴ ᴍᴅ";
+        let pack = Config.STICKER_NAME;
         
         if (mime === "imageMessage" || mime === "stickerMessage") {
             let media = await mek.quoted.download();
@@ -77,4 +77,4 @@ cmd(
     }
 );
 
-// JawadTechX
+// DybyTech 🤍
